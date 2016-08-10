645158f233f769cab2cd1ba3f62cf1d85f1ebbb9
zpzhangwenhan <858966776@qq.com>
2016-08-09 23:56:46 -0700

    烦了 fuck！

--- Xcode_C/helloworld/指针/main.c
+++ Xcode_C/helloworld/指针/main.c
@@ -7,20 +7,16 @@
 //
 
 #include <stdio.h>
+#include <math.h>
 
 int main(int argc, const char * argv[]) {
-    
     int i=99;
     int *i_p=&i;
     double d=3.1415926;
     double *d_p=&d;
     
-    printf("i=%d   i_p=%X --d=%f   d_p=%X \n",(unsigned)i,i_p,d,d_p);
-    
-    
+    printf("i=%d   i_p=%X  d=%f   d_p=%x \n",(unsigned)i,i_p,d,d_p);
     printf("i=%d  i_p=%X  \n",*i_p,&*i_p);
-    
-    
     for (int i=1; i<=9; i++) {
         for (int  j=1; j<=i; j++) {
             printf("%d * %d = %d \t",i,j,i*j);
@@ -28,6 +24,13 @@ int main(int argc, const char * argv[]) {
         printf("\n");
     }
     
+    printf("-----------------------------------------\n");
+    int digite[]={1,2,3,4,5,6,7,8,9,10};
+    int *digite_p=&digite;
+    for (int i=0; i<10; i++) {
+        printf("%d \t",digite[i]);
+    }
+    printf("\n");
     
     return 0;
 }
